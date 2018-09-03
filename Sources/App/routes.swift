f9a0facefa136cdff97edd4e117d2cd73c210c3a
Jeremy Cartee <Jeremy.Cartee@Lowes.com>
2018-09-03 16:35:21 -0400

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,15 +1,42 @@
 import Vapor
 
+struct JSONExample: Content {
+    let name: String
+    let birthday: Date
+    
+}
+
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("json") { req in
+        return JSONExample(name: "Jerm",
+                           birthday: Date())
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    router.get("todos",
+               use: todoController.index)
+    router.get("todos",
+               Todo.parameter,
+               use: todoController.view)
+    router.post("todos",
+                use: todoController.create)
+    router.patch("todos",
+                 Todo.parameter,
+                 use: todoController.update)
+    router.delete("todos",
+                  use: todoController.clear)
+    router.delete("todos",
+                  Todo.parameter,
+                  use: todoController.delete)
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hi \(name)"
+    }
 }
